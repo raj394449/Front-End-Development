b36967bf23a353b8cfb6337822d6c8a9c3d4a827
Sanjay Kumar <132847965+raj394449@users.noreply.github.com>
2024-04-03 16:19:51 +0530

    Day Zero Completed

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ console.log("Hello Worl!");
 window.alert(`Hello World!`);
 
 /*
-    JavaScript : JavaScript is Scripting language that is used to create & control dynamic website content.
+    JavaScript : 
+    
+        *   JavaScript is Scripting language that is used to create & control dynamic website content.
+        *   JavaScript can calculat, manipulate & validate data.
+        *   JavaScript can update & change both HTML & CSS
+    
+    
 */

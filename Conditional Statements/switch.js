60a6ea9fe7959e9b159e2a5c97a55e1a48779d8d
Sanjay Kumar <132847965+raj394449@users.noreply.github.com>
2024-05-07 17:02:45 +0530

    After comming from home

---
+++ Conditional Statements/switch.js
@@ -0,0 +1,11 @@
+let day = 4;
+switch (day) {
+    case 1: console.log(` It is monday`); break;
+    case 2: console.log(`It is Tuesday`); break;
+    case 3: console.log(`It is Wednesday`); break;
+    case 4: console.log(`It is Thrusday`); break;
+    case 5: console.log(`It is Friday`); break;
+    case 6: console.log(`It is Saturday`); break;
+    case 7: console.log(`It is Sunday`); break;
+    default: console.log(`${day} Not a Day`)
+}
---
+++ Strings/script.js
@@ -0,0 +1,42 @@
+userName = 'SanjayRaj'
+
+// // 1. charAt() Method
+// console.log('Character at index zero : ', userName.charAt(0));
+
+// // 2. indexOf() Method
+// console.log('The index of R is : ', userName.indexOf('R'));
+
+// // 3. lastIndexOf() Method
+// console.log('The last index of a is : ', userName.lastIndexOf('a'));
+
+// // 4. lenght() Method
+// console.log('The length of userName is : ', userName.length)
+
+// // 5. trim() Method
+// console.log('Before using trim method : ', userName);
+// console.log('After using trim Method : ', userName.trim())
+
+// // 6. toUpperCase() Method
+// console.log(userName.toUpperCase());
+
+// // 7. toLowerCase() Method
+// console.log(userName.toLowerCase())
+
+// // 8. repeat() Method
+// console.log(userName.repeat(4))
+
+// // 9. startsWith() Method
+// let result = userName.startsWith('S')
+// console.log(result)
+
+// // 10. endsWith() Method
+// let result = userName.endsWith('J')
+// console.log(result)
+
+// // 11. replaceAll()  & replace() Method
+// let phoneNumber = '123-456-7897'
+// // phoneNumber = phoneNumber.replaceAll('-', '')
+// // console.log(phoneNumber)
+// console.log(phoneNumber.replace('-', '*'))
+
+// 12. padStart() & padEnd() Method 
